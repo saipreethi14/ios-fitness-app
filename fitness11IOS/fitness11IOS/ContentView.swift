80358fc94aba8a6919077653430c3885c113919e
praveenk111 <58185797+praveenk111@users.noreply.github.com>
2021-12-23 14:06:45 -0500

    Add files via upload
    done

---
+++ fitness11IOS/fitness11IOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fitness11IOS
+//
+//  Created by Vinith kumar Tatipally on 2021-12-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fitness11IOS/fitness11IOS/HomePage.swift
@@ -0,0 +1,20 @@
+//
+//  HomePage.swift
+//  fitness11IOS
+//
+//  Created by Vinith kumar Tatipally on 2021-12-23.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomePage_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePage()
+    }
+}
