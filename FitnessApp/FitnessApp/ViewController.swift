717c3dedb3fc13f22683ff629e69df4cc54b6224
praveenk111 <58185797+praveenk111@users.noreply.github.com>
2021-12-23 12:22:54 -0500

    Add files via upload
    done with sign in sign up and listofexercise pages

---
+++ FitnessApp/FitnessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnessApp
+//
+//  Created by Vinith kumar Tatipally on 2021-12-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
